6010c69510ef2e237f6bfa7620b1983ba01966cc
Khairallah AL-Awady <khairallahfreelancer@gmail.com>
2024-09-29 18:54:24 +0300

    feat: ✨ Update page interface (#1)
    Co-authored-by: EngineerEmmanuel <EngineerEmmanuel@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,103 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import styles from './index.module.css';
-import Card from "../components/Card";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
+import Box from "../components/Box";
 
 function Home() {
-    const context = useDocusaurusContext();
-    const { siteConfig = {} } = context;
-  
-    return (
-      <Layout title="Homepage" description="PWR Chain Documentation">
-        <main className={styles.main}>
-          <br />
-          <section className={styles.features}>
-            <div className="container">
-              <h1
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
+  return (
+    <Layout title="Homepage" description="PWR Chain Documentation">
+      <main className={styles.main}>
+        <br />
+        <section className={styles.features}>
+          <div className="container">
+            <h2
               title="tagline"
               className="landing-tagline"
               style={{
                 fontWeight: "700",
                 marginBottom: "0px",
-                fontSize: "x-large",
                 paddingTop: "55px",
                 paddingBottom: "10px",
+                textAlign: "center",
               }}
             >
-              Welcome to the PWR Chain Documentation Site
-            </h1>
-              <div className="row cards__container docs-landing">
-                <Card
-                  to="pwrchain/overview"
-                  header={{
-                    label:"Intro to PWR Chain"
-                  }}
-                  body={{
-                    label:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget vulputate neque. Maecenas sit amet ornare augue"
-                  }}
-                />
-  
-                <Card
-                  to="/"
-                  header={{
-                    label:"Guides"
-                  }}
-                  body={{
-                    label:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget vulputate neque. Maecenas sit amet ornare augue"
-                  }}
-                />
-  
-                <Card
-                  to="/"
-                  header={{
-                    label:"Guides for PWR"
-                  }}
-                  body={{
-                    label:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget vulputate neque. Maecenas sit amet ornare augue"
-                  }}
-                />
-              </div>
-            </div>
-          </section>
-        </main>
+              Welcome to the PWR Chain Documentation
+            </h2>
+            <div className="landing-page-boxes-con">
+              <Box
+                to="pwrchain/overview"
+                header={{
+                  label: "Intro to PWR Chain",
+                }}
+                body={{
+                  label:
+                    "Start your journey with an overview of the protocol including key terms, and features.",
+                }}
+              />
 
-      </Layout>
-    );
-  }
-  
-  export default Home;
+              <Box
+                to="pwrchain/architecture-of-pwr-chain/base-layer"
+                header={{
+                  label: "Architecture of PWR",
+                }}
+                body={{
+                  label:
+                    "Explains the infrastructure and network design, covering how nodes, and data flow operate.",
+                }}
+              />
+
+              <Box
+                to="pwrchain/core-technology/consensus-mechanism/proof-of-power"
+                header={{
+                  label: "Core Technology",
+                }}
+                body={{
+                  label:
+                    "Highlights the core technological components that power the PWR Chain.",
+                }}
+              />
+
+              <Box
+                to="developers/developing-on-pwr-chain/what-is-a-decentralized-application"
+                header={{
+                  label: "Guides for Developers",
+                }}
+                body={{
+                  label:
+                    "Step-by-step instructions for developers to build, and deploy on PWR Chain.",
+                }}
+              />
+
+              <Box
+                to="developers/sdks/installing-and-importing-pwr-sdk"
+                header={{
+                  label: "PWR Chain SDKs",
+                }}
+                body={{
+                  label:
+                    "A comprehensive guide to the PWR Chain SDKs, and how to build DApps and VMs.",
+                }}
+              />
+
+              <Box
+                to="pwrchain/governance-and-economics/tokenomics/pwr-utility-and-value"
+                header={{
+                  label: "Governance",
+                }}
+                body={{
+                  label:
+                    "Details on the governance model, tokenomics, and how economic incentives are structured.",
+                }}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
